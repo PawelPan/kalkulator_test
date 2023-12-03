84b4f2b19eddf83e0a9815b470221119a0541220
PawelPan <ppanczyk24@gmail.com>
2023-12-03 15:13:00 +0100

    dodawanie

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+def Add():
+    inp1 = int(input("first number:"))
+    inp2 = int(input("second number:"))
+    print("result: ", inp1 + inp2)
+
 flag = True
+
 while flag:
+    opt = int(input("choose option\n1 addition\n"))
+    if opt == 1:
+        Add()
+    else:
+        continue
     inp = str(input("continue? [y/n]:"))
     flag = inp == 'y'
+
 
 
