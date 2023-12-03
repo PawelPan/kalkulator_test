cf31e69d2efafaa3d11a8e92dd304f880b07062a
PawelPan <ppanczyk24@gmail.com>
2023-12-03 15:30:19 +0100

    odejmowanie

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ def Add():
     inp2 = int(input("second number:"))
     print("result: ", inp1 + inp2)
 
+def Sub():
+    inp1 = int(input("first number:"))
+    inp2 = int(input("second number:"))
+    print("result: ", inp1 - inp2)
+
 flag = True
 while flag:
-    opt = int(input("choose option\n1 addition\n"))
+    opt = int(input("choose option\n1 addition\n2 substraction"))
     if opt == 1:
         Add()
+    elif opt == 2:
+        Sub()
     else:
         continue
     inp = str(input("continue? [y/n]:"))
